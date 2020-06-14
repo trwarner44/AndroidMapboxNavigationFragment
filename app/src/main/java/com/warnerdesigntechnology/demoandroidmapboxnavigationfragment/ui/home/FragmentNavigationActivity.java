48013e63c0ddc0afab4f84e0580dabf526ad4781
Tommy Warner <tommy.r.warner@gmail.com>
2020-06-14 11:28:49 -0400

    Updated java files to fix error codes

---
+++ app/src/main/java/com/warnerdesigntechnology/demoandroidmapboxnavigationfragment/ui/home/FragmentNavigationActivity.java
@@ -0,0 +1,82 @@
+package com.warnerdesigntechnology.demoandroidmapboxnavigationfragment.ui.home;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.warnerdesigntechnology.demoandroidmapboxnavigationfragment.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class FragmentNavigationActivity extends AppCompatActivity {
+
+  private static final String FAB_VISIBLE_KEY = "restart_fab_visible";
+
+  @BindView(R.id.restart_navigation_fab)
+  FloatingActionButton restartNavigationFab;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_navigation_fragment);
+    ButterKnife.bind(this);
+    initializeNavigationViewFragment(savedInstanceState);
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putBoolean(FAB_VISIBLE_KEY, restartNavigationFab.getVisibility() == View.VISIBLE);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    boolean isVisible = savedInstanceState.getBoolean(FAB_VISIBLE_KEY);
+    int visibility = isVisible ? View.VISIBLE : View.INVISIBLE;
+    restartNavigationFab.setVisibility(visibility);
+  }
+
+  @OnClick(R.id.restart_navigation_fab)
+  public void onClick(FloatingActionButton restartNavigationFab) {
+    replaceFragment(new HomeFragment());
+    restartNavigationFab.hide();
+  }
+
+  public void showNavigationFab() {
+    restartNavigationFab.show();
+  }
+
+  public void showPlaceholderFragment() {
+    replaceFragment(new PlaceholderFragment());
+  }
+
+  private void initializeNavigationViewFragment(@Nullable Bundle savedInstanceState) {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    if (savedInstanceState == null) {
+      FragmentTransaction transaction = fragmentManager.beginTransaction();
+      transaction.disallowAddToBackStack();
+      transaction.add(R.id.navigation_fragment_frame, new HomeFragment()).commit();
+    }
+  }
+
+  private void replaceFragment(Fragment newFragment) {
+    String tag = String.valueOf(newFragment.getId());
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    transaction.disallowAddToBackStack();
+    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+    int fadeInAnimId = androidx.appcompat.R.anim.abc_fade_in;
+    int fadeOutAnimId = androidx.appcompat.R.anim.abc_fade_out;
+    transaction.setCustomAnimations(fadeInAnimId, fadeOutAnimId, fadeInAnimId, fadeOutAnimId);
+    transaction.replace(R.id.navigation_fragment_frame, newFragment, tag);
+    transaction.commit();
+  }
+}
